dbff3635088048ee0823f20de7a4e95cb0927914
Ishika Jain <75484706+the-ishikajain@users.noreply.github.com>
2023-02-16 23:36:15 +0530

    Create Doctor.java

---
+++ Doctor.java
@@ -0,0 +1,19 @@
+class Doctor extends Hospital
+{
+    
+    Doctor()
+    {
+        super("hello");
+    }   
+   
+    public String toString()
+    {
+    System.out.println("Doctor Name : " + super.doctorName);
+    System.out.println("Doctor Speciality : " + super.doctorSpeciality);
+    System.out.println("Doctor Payment : " + super.doctorPayment);
+    System.out.println("Doctor Age : " + super.doctorAge);
+    return "\n";
+    }
+    
+
+}
