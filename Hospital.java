721f68e6f823a86b9563b5500e518c07083d8298
Ishika Jain <75484706+the-ishikajain@users.noreply.github.com>
2023-02-21 11:20:46 +0530

    Update Hospital.java

--- Hospital.java
+++ Hospital.java
@@ -13,27 +13,42 @@ class Hospital
     int doctorAge;
    Hospital()
     {
+      try
+      {
        System.out.println("Enter payment of a patient : ");
         this.patientPayment=new Scanner(System.in).nextInt();
         System.out.println("Enter name of a patient : ");
-        this.patientName= new Scanner(System.in).next();
+        this.patientName= InvalidNameException.readName();;
         System.out.println("Enter age of a patient : ");
-        this.patientAge=new Scanner(System.in).nextInt();
+        this.patientAge=InvalidAgeException.readAge();
         System.out.println("Enter disease of a patient : ");
         this.disease = new Scanner(System.in).next();
         this.designation_p="Patient";
+      }
+      catch(Exception e)
+      {
+         System.out.println(e);
+      }
+      
     } 
     Hospital(String name)
     {
+       try
+       {
       System.out.println("Enter payment of a doctor : ");
         this.doctorPayment=new Scanner(System.in).nextInt();
         System.out.println("Enter name of a doctor : ");
-        this.doctorName= new Scanner(System.in).next();
+        this.doctorName= InvalidNameException.readName();;
         System.out.println("Enter speciality of a doctor : ");
         this.doctorSpeciality=new Scanner(System.in).next();
         System.out.println("Enter age of a doctor : ");
-        this.doctorAge = new Scanner(System.in).nextInt();
+        this.doctorAge = InvalidAgeException.readAge();
         this.designation_d="Doctor";
+       }
+       catch
+       {
+           System.out.println(e);
+       }
     } 
   
    
