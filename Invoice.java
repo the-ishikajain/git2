8596a727097bb3420374831a0dffc6ec7404db0a
Ishika Jain <75484706+the-ishikajain@users.noreply.github.com>
2023-02-26 21:15:35 +0530

    Update Invoice.java

--- Invoice.java
+++ Invoice.java
@@ -1,18 +1,7 @@
 package emp1;
 import java.util.*;
-class Invoice extends Hopital
+class Invoice 
 {
-int id;
-int patient_id;
-int amount;
-Invoice(int patientPayment)
-{
-amount=patientPayement;
-this.id=new Scanner(System.in).nextInt();
-this.patient_id=new Scanner(System.in).nextInt();
-}
-public String toString()
-{
-return "id : " + id + " patient_id :"  patient_id + "amount : " + amount;
+
 }
 }
