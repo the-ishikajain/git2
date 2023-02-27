64b45d92da04c4a8735d585b919903c4942edbd9
Ishika Jain <75484706+the-ishikajain@users.noreply.github.com>
2023-02-27 10:58:30 +0530

    Update Invoice.java

--- Invoice.java
+++ Invoice.java
@@ -1,7 +1,14 @@
 package emp1;
 import java.util.*;
-class Invoice 
+class Invoice extends Hospital
 {
-
+  int p_id;
+  Invoice(int patientPayment)
+  {
+     this.p_id=patientPayment;
+    int id=sc.nextInt();
+  int patient_id=sc.nextInt();
+   
+  }
 }
 }
